075795df7a69d4ce89db92e2d90dc1194fb9c7f9
dylanbram354 <dylanbram354@gmail.com>
2021-06-02 15:15:30 -0500

    Created 'new_superhero' page and add view.create function

--- superhero_database/superheroes/urls.py
+++ superhero_database/superheroes/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 app_name = 'superheroes'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:superhero_id>/', views.details, name='details'),
+    path('new_superhero/', views.create, name='create_superhero')
 ]
--- superhero_database/superheroes/views.py
+++ superhero_database/superheroes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Superhero
+from django.urls import reverse
 # Create your views here.
 
 
@@ -10,3 +11,26 @@ def index(request):
         'all_superheroes': all_superheroes
     }
     return render(request, 'superheroes/index.html', context)
+
+def details(request, superhero_id):
+    # hero = Superhero.objects.get(id=superhero_id)
+    # context = {
+    #     'hero': hero
+    # }
+    # return render(request, *details.html (doesn't exist yet)*, context)
+    return HttpResponse('Placeholder text')
+    pass
+
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary_ability = request.POST.get('primary_ability')
+        secondary_ability = request.POST.get('secondary_ability')
+        catchphrase = request.POST.get('catchphrase')
+        new_superhero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary_ability,
+                                  secondary_ability=secondary_ability, catchphrase=catchphrase)
+        new_superhero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/create.html')
